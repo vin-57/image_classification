280eb9fc5b984487ee416194a8fc66befe28c411
Ivan Ivanov <vin_57@mail.ru>
2022-11-19 22:54:26 +0300

    Added recognition

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
 import io
 import streamlit as st
 from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 
 def load_image():
-    """Создание формы для загрузки изображения"""
-    # Форма для загрузки изображения средствами Streamlit
-    uploaded_file = st.file_uploader(
-        label='Выберите изображение для распознавания')
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
-        # Получение загруженного изображения
         image_data = uploaded_file.getvalue()
-        # Показ загруженного изображения на Web-странице средствами Streamlit
         st.image(image_data)
-        # Возврат изображения в формате PIL
         return Image.open(io.BytesIO(image_data))
     else:
         return None
 
-# Выводим заголовок страницы средствами Streamlit     
-st.title('Классификация изображений')
-# Вызываем функцию создания формы загрузки изображения
+
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+model = load_model()
+
+
+st.title('Классификации изображений в облаке Streamlit')
 img = load_image()
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
